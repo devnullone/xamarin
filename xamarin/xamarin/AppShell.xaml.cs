bc88fb49fec6790f2c7d0569e23b4fe4a45850e4
KODZO A. Mawuena <mawuenamichelk@icloud.com>
2022-05-03 01:38:41 +0000

    Ajoutez des fichiers projet.

---
+++ xamarin/xamarin/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace xamarin
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
